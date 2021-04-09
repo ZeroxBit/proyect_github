ba47fd9df3bdbb4f67ebf1085acaaf4b3f8e0d89
Carlos Sanchez <xt3426@rimac.com.pe>
2021-04-09 18:28:06 -0500

    [ADD] agregado y configurado el dotenv

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import React, { useEffect } from "react"
+import env from "react-dotenv";
 
 function App() {
-
+    console.log("url", env.API_URL)
     useEffect(() => {
 
-        axios.get("https://api.github.com/users/").then(value => console.log("value", value)).catch(err => console.log("error", err))
+        axios.get("https://api.github.com/users").then(value => console.log("value", value)).catch(err => console.log("error", err))
 
     }, [])
 
