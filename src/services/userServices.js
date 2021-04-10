29d6ee5fa57bde5ac116185ef82825372e38f3df
Carlos Sanchez <xt3426@rimac.com.pe>
2021-04-09 19:15:21 -0500

    [CREATE] creado servicios para los usuarios

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,7 +1,19 @@
 import { API } from "./connection"
 import env from "react-dotenv";
 
+/**
+ * No necesita recibir parametos
+ * @returns Regresa un listado de usuarios
+ */
 export const getUsersServices = async () => {
-    console.log( "apth", env.PATH_USERS )
     return await API.get(env.PATH_USERS);
+}
+
+/**
+ * Recibe el parametro obligatorio y regresa un unico usuario!!
+ * @param {string} name El nombre de usuario a buscar
+ * @returns Regresa el usuario buscado
+ */
+export const getUserByName = async (name) => {
+    return await API.get(env.PATH_USER, name);
 }
