a830be79b585ecf663313035342761c2ee0f0922
Carlos Sanchez <xt3426@rimac.com.pe>
2021-04-11 12:22:10 -0500

    [CHANGE] editando el app component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,9 @@
-import React, { useEffect, useState } from "react";
-import env from "react-dotenv";
-import { NavBar } from "./components/navBar/NavBar";
-import { getSearchUsersServices } from "./services/userServices";
+import React from "react";
 import "./style/style.scss";
+import Users from "./views/Users";
 
 function App() {
-    console.log("url", env.API_URL);
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        // getUsersServices()
-        //     .then((value) => setData(value.data))
-        //     .catch((err) => console.log("error", err));
-    }, []);
-
-    const handleChange = async (e) => {
-        const name = e.target.value;
-        getSearchUsersServices(name)
-            .then((result) => {
-                console.log("resultt", result.data);
-            })
-            .catch((err) => {
-                console.log("errorr", err);
-            });
-    };
-
-    console.log("data", data);
-    return (
-        <>
-            <div className="container">
-                <div className="column is-12">
-                    <input
-                        className="input is-normal"
-                        type="text"
-                        placeholder="Normal input"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="column is-3">
-                    <div className="card">
-                        <div className="card-image">
-                            <figure className="image is-4by3">
-                                <img src={""} alt="Placeholder image" />
-                            </figure>
-                        </div>
-                        <div className="card-content">
-                            <div className="media-content">
-                                <p className="title is-4">@ Krlos</p>
-                            </div>
-                        </div>
-                        <footer className="card-footer">
-                            <a href="#" className="card-footer-item">
-                                Conoceme
-                            </a>
-                            <a href="#" className="card-footer-item">
-                                Repos
-                            </a>
-                            <a href="#" className="card-footer-item">
-                                Eliminar
-                            </a>
-                        </footer>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+    return <Users />;
 }
 
 export default App;
