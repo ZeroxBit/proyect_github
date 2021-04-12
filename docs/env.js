7a6148c90101db0055e3b49fd2348b2770482a54
Carlos Sanchez <xt3426@rimac.com.pe>
2021-04-11 23:38:13 -0500

    [CREATE] creando carpeta docs

---
+++ docs/env.js
@@ -0,0 +1,8 @@
+window.env = {
+  "API_URL": "https://api.github.com",
+  "PATH_USERS": "/users",
+  "PATH_REPOSITORIES": "/search/repositories",
+  "PATH_SEARCH_USERS": "/search/users",
+  "HEADER": "application/vnd.github.v3+json",
+  "SRC_LOGO": "https://luuna-bucket.imgix.net/img/header-logo.svg?auto=compress,format"
+};
