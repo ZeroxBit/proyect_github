ae85acee2035cebca858feb710842418d922ab25
Carlos Sanchez <djkrlosmix1@gmail.com>
2022-03-10 15:23:27 -0500

    [UPDATE] version 2.0

---
+++ public/env-LAPTOP-3F9N1SCA.js
@@ -0,0 +1,9 @@
+window.env = {
+  "API_URL": "http://localhost:8080",
+  "PATH_USERS": "/users",
+  "PATH_REPOSITORIES": "/api/auth/repo",
+  "PATH_SEARCH_USERS": "/api/auth/users",
+  "HEADER": "application/vnd.github.v3+json",
+  "SRC_LOGO": "https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png",
+  "PATH_LOGIN": "/api/auth/login"
+};
